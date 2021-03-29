7c810e49f82513f449d8f2b87c730c28be018f2a
Chuong Tran <chuongninolxag@gmail.com>
2021-03-29 21:06:29 +0700

    update route

--- research-style/src/routes.js
+++ research-style/src/routes.js
@@ -1,10 +1,11 @@
 import { lazy } from 'react';
 
 const PageDemo = lazy(() => import('./components/pages/Demo/PageDemo'));
-
+const NotFound = lazy(() => import('./components/pages/NotFound'));
 
 export const Routes = [
-    { id: '/page-demo', component: PageDemo, exact: true },
+    { id: '/', component: PageDemo, exact: true },
+    {id: '*', component: NotFound}
 ];
 
 
